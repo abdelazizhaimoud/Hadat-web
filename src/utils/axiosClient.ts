90d45b20f9d760941570ebbcc7ed4c55587a539c
Abdelaziz HAIMOUD <abdelazizhaimoud@outlook.com>
2026-03-11 14:58:35 +0000

    add axios instance with response interceptors and update dependencies

---
+++ src/utils/axiosClient.ts
@@ -0,0 +1,46 @@
+import axios, { type AxiosInstance, type AxiosResponse, type AxiosError } from 'axios';
+
+const BASE_URL = "http://localhost:8000/api"
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+});
+
+axiosInstance.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    if (error.response) {
+      switch (error.response.status) {
+        case 401:
+          console.log('Unauthorized access');
+          break;
+        case 403:
+          console.log('Forbidden access');
+          break;
+        case 404:
+          console.log('Resource not found');
+          break;
+        case 500:
+          console.log('Server error');
+          break;
+        default:
+          console.log('An error occurred');
+      }
+    } else if (error.request) {
+      console.log('No response received from server');
+    } else {
+      console.log('Error setting up request:', error.message);
+    }
+    
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
