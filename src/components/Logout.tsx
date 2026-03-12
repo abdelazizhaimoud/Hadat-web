64a0768677524c56e6ee1a76b4b62a88450bb9cc
Abdelaziz HAIMOUD <abdelazizhaimoud@outlook.com>
2026-03-12 15:57:04 +0000

    implement logout functionality

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,10 +1,28 @@
+import { useEffect } from 'react'
+import axiosInstance from '../utils/axiosClient'
 import Header from './home/Header'
 
 function Logout() {
+  const handleLogout = async () => {
+    try{
+      const response = await axiosInstance.post('/logout')
+      if (response.status == 200){
+        console.log(response.data.message)
+      }
+    }catch (error){
+      console.log(error)
+    }finally{
+      localStorage.removeItem('auth-token')
+      window.location.href = "/login"
+    }
+  }
+  useEffect(()=>{
+    handleLogout()
+  },[])
   return (
     <div>
         <Header></Header>
-      logout
+      logging out .....
     </div>
   )
 }
